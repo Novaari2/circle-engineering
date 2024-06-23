f4b982ca5ccf0fee501f8adb572adf3c89e12254
novaari2 <novaari20@gmail.com>
2024-06-23 10:35:35 +0700

    perbaikan metadata dan layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -8,20 +8,20 @@ import Head from "next/head";
 const outfit = Outfit({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Circle Engineering",
-  description: "Jasa Perbaikan Dan Instalasi Listrik Semarang",
+  title: "Jasa Perbaikan Dan Instalasi Listrik Semarang",
+  description: "Jasa Perbaikan Dan Instalasi Listrik Di Kota Semarang Dan Jawa Tengah. Semua Jenis Instalasi Listrik: rumah tangga, pabrik, perkantoran, dan lainnya.",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="id" suppressHydrationWarning>
       <Head>
         <meta name="google-site-verification" content="JJnPlHsHdheBoVTvxJTeBU25MPQPys9ZcSY-9_Lzt98" />
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
       </Head>
       <body className={outfit.className}>
-        <ThemeProvider attribute='class' defaultTheme='light'>
+        <ThemeProvider attribute="class" defaultTheme="light">
           <Header />
           {children}
         </ThemeProvider>
